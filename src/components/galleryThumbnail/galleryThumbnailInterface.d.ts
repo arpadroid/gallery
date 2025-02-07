5cd2b7f0a1e4747d8c8d86a9275e284c45f6ce97
Andres Vaquero <web.andresvaquero@gmail.com>
2025-02-07 22:17:52 +1100

    working on types

--- src/components/galleryThumbnail/galleryThumbnailInterface.d.ts
+++ src/components/galleryThumbnail/galleryThumbnailInterface.d.ts
@@ -1,5 +1,5 @@
-import { ListItemInterface } from '@arpadroid/lists/src/components/listItem/listItemInterface';
+import { ListItemConfigType } from '@arpadroid/lists/src/components/listItem/listItemInterface';
 
-export interface GalleryThumbnailInterface extends ListItemInterface {
+export interface GalleryThumbnailInterface extends ListItemConfigType {
     someProp: string;
 }
