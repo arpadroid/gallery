e1eb6db360254001c2eba277b290f386915b8d5e
Andres Vaquero <web.andresvaquero@gmail.com>
2026-02-10 21:25:40 +1100

    Refactoring test runner logic and simplifying configuration handling, specially for Jest but also for Storybook

--- src/arpadroid.config.js
+++ src/arpadroid.config.js
@@ -1,7 +1,9 @@
+/** @type {import("@arpadroid/module").BuildConfigType} */
 const config = {
     deps: ['forms', 'lists', 'navigation', 'ui'],
     buildTypes: true,
     buildType: 'uiComponent',
+    storybook_port: 6015,
     logo: `           ┓    • ┓      ┓┓      
   ┏┓┏┓┏┓┏┓┏┫┏┓┏┓┓┏┫  ┏┓┏┓┃┃┏┓┏┓┓┏
   ┗┻┛ ┣┛┗┻┗┻┛ ┗┛┗┗┻  ┗┫┗┻┗┗┗ ┛ ┗┫
