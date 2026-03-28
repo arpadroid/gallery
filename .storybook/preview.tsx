00a828d2218b1dfd5b67ce142d5334895b589333
Andres Vaquero <web.andresvaquero@gmail.com>
2026-03-29 10:38:52 +1100

    porting storybook preview and main files to typescript

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,7 +4,6 @@
  * Add your Storybook preview configuration overrides here if needed.
  */
 import PreviewConfig from '@arpadroid/module/storybook/preview';
-/** @type { import('@storybook/web-components-vite').Preview } */
 import { bootstrapDecorator } from '@arpadroid/module/storybook/decorators';
 import { setService } from '@arpadroid/context';
 import { Router, APIService } from '@arpadroid/services';
